d26deffd638433779a9c878b5457ff3e193e88f3
priyanshu <priyanshu.23bora@gmail.com>
2025-07-13 01:55:48 +0530

    static landing page

---
+++ client/src/app/(nondashboard)/landing/HeroSection.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const HeroSection = () => {
+  return (
+    <div className="relative h-screen">
+      <Image
+        src="/landing-splash.jpg"
+        alt="Rentiful Rental Platform Hero Section"
+        fill
+        className="object-cover object-center"
+        priority
+      />
+      <div className="absolute inset-0 bg-black bg-opacity-60"></div>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        className="absolute top-1/3 transform -translate-x-1/2 -translate-y-1/2 text-center w-full"
+      >
+        <div className="max-w-4xl mx-auto px-16 sm:px-12">
+          <h1 className="text-5xl font-bold text-white mb-4">
+            Find your ideal home today.
+          </h1>
+          <p className="text-xl text-white mb-8">
+            Discover rental spaces designed to match your lifestyle, budget, and
+            dreams!
+          </p>
+
+          <div className="flex justify-center">
+            <Input
+              type="text"
+              placeholder="Search by city, neighborhood or address"
+              className="w-full max-w-lg rounded-none rounded-l-xl border-none bg-white h-12"
+            />
+            <Button className="bg-secondary-500 text-white rounded-none rounded-r-xl border-none hover:bg-secondary-600 h-12">
+              Search
+            </Button>
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import HeroSection from './HeroSection'
+import FeaturesSection from './FeaturesSection'
+import DiscoverSection from './DiscoverSection'
+import FooterSection from './FooterSection'
+import CallToActionSection from './CallToActionSection'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div>
+      <HeroSection />
+      <FeaturesSection />
+      <DiscoverSection />
+      <CallToActionSection />
+      <FooterSection />
+    </div>
+    
   )
 }
 
