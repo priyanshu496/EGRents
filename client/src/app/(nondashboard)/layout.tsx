e88f9eadbafc4c2ecf73fa07371909f5a83136b7
priyanshu <priyanshu.23bora@gmail.com>
2025-07-13 00:42:41 +0530

    Navbar

---
+++ client/src/app/(nondashboard)/layout.tsx
@@ -0,0 +1,20 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full w-full">
+      <Navbar />
+     
+      <main
+        className={`h-full flex w-full flex-col`}
+        style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
+      >
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default layout
