a949e1d50f62c59580ad911274a040f84d471e8b
priyanshu <priyanshu.23bora@gmail.com>
2025-07-16 23:26:32 +0530

    application routes and controller

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,8 @@ import morgan from "morgan";
 import { authMiddleware } from "./middleware/authMiddleware";
 import tenantRoutes from "./routes/tenantRoutes";
 import managerRoutes from "./routes/managerRoutes";
-
+import propertyRoutes from "./routes/propertyRoutes";
+import applicationRoutes from './routes/applicationRoutes'
 /* CONFIGURATIONS */
 dotenv.config();
 const app = express();
@@ -24,7 +25,9 @@ app.get("/", (req, res) => {
   res.send("This is home route");
 });
 
-
+app.use("/applications", applicationRoutes);
+app.use("/properties", propertyRoutes);
+// app.use("/leases", leaseRoutes);
 app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
 app.use("/managers", authMiddleware(["manager"]), managerRoutes);
 
